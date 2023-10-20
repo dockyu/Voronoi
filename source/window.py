b8cb5108633215e06e15c0dbb5582aebb925878e
dockyu <dockyu.chen@gmail.com>
2023-10-20 22:16:36 +0800

    window appearance

--- source/window.py
+++ source/window.py
@@ -10,6 +10,10 @@ def on_run_clicked():
 def on_step_by_step_clicked():
     print("Step by Step button clicked")
 
+def on_clear_clicked():
+    ax.clear()  # 清除 matplotlib 畫布
+    canvas.draw()  # 重新繪制畫布
+
 def import_input_file():
     print("Imported Input File")
 
@@ -33,9 +37,15 @@ file_menu.add_command(label="Import Output File", command=import_output_file)
 file_menu.add_command(label="Export Output File", command=export_output_file)
 
 run_menu = tk.Menu(menu_bar, tearoff=0)
-menu_bar.add_cascade(label="Run", menu=run_menu)
+menu_bar.add_cascade(label="run", menu=run_menu)
 run_menu.add_command(label="Run", command=on_run_clicked)
 run_menu.add_command(label="Step by Step", command=on_step_by_step_clicked)
+
+# 在建立 run_menu 之後添加
+clear_menu = tk.Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="clear", menu=clear_menu)
+clear_menu.add_command(label="Clear", command=on_clear_clicked)
+
 
 canvas_frame = tk.Frame(root)
 canvas_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
